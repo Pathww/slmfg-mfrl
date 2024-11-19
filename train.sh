4c42d38c3cee15b375c0127d278b782336e6b1c5
linhaowang <22373399@buaa.edu.cn>
2024-11-19 11:03:49 +0800

    feat: train adv

--- train.sh
+++ train.sh
@@ -1,2 +1,4 @@
 python train.py --device 1 --alg dqn --use_mf True
-python evaluate.py --device 1 --alg ppo --agent-num 150 --use_mf True --init-checkpoint 2000000 --checkpoint-dir ppo-render/e-150
+python evaluate.py --device 1 --alg ppo --agent-num 150 --use_mf True --init-checkpoint 2000000 --checkpoint-dir checkpoint
+python train_adv.py --device 1 --alg ppo --init-checkpoint 2000000 --checkpoint-dir checkpoint
+python train_adv.py --device 1 --alg ppo --use_mf True --init-checkpoint 2000000 --checkpoint-dir checkpoint
